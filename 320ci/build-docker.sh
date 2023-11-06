d8b1cdd8bb497968ed30fdb26c76edb71d65bdce
Aleksandar Ivanov <92474254+sadjokerwin@users.noreply.github.com>
2023-11-06 22:59:03 +0200

    Delete 320ci directory

--- 320ci/build-docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-[[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}"
-[[ -z "${sadjokerwin}" ]] && DockerRepo='' || DockerRepo="ghcr.io/${sadjokerwin}/"
-docker build -t "${DockerRepo}micronaut-app:latest" -t "${DockerRepo}micronaut-app:1.0-$Tag" .  
